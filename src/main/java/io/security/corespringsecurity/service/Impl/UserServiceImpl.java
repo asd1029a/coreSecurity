b2eb30f53fce7a7caef1e42713fab686f07dba85
asd1029a <xxx1029x@gmail.com>
2022-12-14 00:45:12 +0900

    web login

---
+++ src/main/java/io/security/corespringsecurity/service/Impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package io.security.corespringsecurity.service.Impl;
+
+import io.security.corespringsecurity.domain.Account;
+import io.security.corespringsecurity.repository.UserRepository;
+import io.security.corespringsecurity.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    @Override
+    public void createUser(Account account) {
+        userRepository.save(account);
+    }
+}
